4bae84e31c3f72a0bf2ffb8c3be74301b1ef308a
Gabriel160204 <gabriel16020412@gmail.com>
2022-11-19 16:46:44 -0300

    icons

--- nvim/lua/cmpConfig.lua
+++ nvim/lua/cmpConfig.lua
@@ -1,3 +1,31 @@
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
 local cmp = require'cmp'
 
 cmp.setup({
@@ -5,9 +33,9 @@ snippet = {
   -- REQUIRED - you must specify a snippet engine
   expand = function(args)
 	vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-	-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-	-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-	-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+	require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+	require('snippy').expand_snippet(args.body) -- For `snippy` users.
+	vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
   end,
 },
 window = {
@@ -21,7 +49,7 @@ mapping = cmp.mapping.preset.insert({
   ['<C-e>'] = cmp.mapping.abort(),
   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   ['<Tab>'] = cmp.mapping(function(fallback)
-		if cmp.visible() then						
+		if cmp.visible() then
 			cmp.select_next_item()
 				return
 		end
@@ -29,7 +57,7 @@ mapping = cmp.mapping.preset.insert({
 	end
 	, { 'i', 'c' }),
   ['<S-Tab>'] = cmp.mapping(function(fallback)
-		if cmp.visible() then							
+		if cmp.visible() then
 			cmp.select_prev_item()
 				return
 			end
@@ -37,6 +65,21 @@ mapping = cmp.mapping.preset.insert({
 		end
 	, { 'i', 'c' }),
 }),
+formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
 sources = cmp.config.sources({
   { name = 'nvim_lsp' },
   { name = 'vsnip' }, -- For vsnip users.
